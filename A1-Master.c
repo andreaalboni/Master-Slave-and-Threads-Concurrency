63e26ec7f23a14d9f47ecd7f1844bf4c171e578b
andreaalboni <andrea.alboni2@studio.unibo.it>
2024-01-17 00:17:14 +0100

    A1 Finished

---
+++ A1-Master.c
@@ -0,0 +1,136 @@
+/*
+ ============================================================================
+ Name        : A1-Master.c
+ Author      : Andrea Alboni
+ Version     : 1
+ Copyright   : For personal use only
+ Description : Template for A1's master program
+ ============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/sem.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <errno.h>
+
+#define TRUE 1
+#define MAX 20
+#define THRESHOLD 100
+
+int terminated; // flag to signal termination
+
+// semaphores
+sem_t *sem_sig;
+sem_t *sem_child_ready;
+sem_t *sem_parent_done;
+
+// signal handler
+void handler(int signo) {
+	if(signo==SIGUSR1) {
+		// send signal to child
+		sem_post(sem_sig);
+	}
+	else {
+		// abort
+		perror("Wrong signal number");
+		abort();
+	}
+}
+
+int main(void) {
+	// create fifo for write
+	if(mkfifo("/tmp/named_pipe", 0666) == -1) {
+		if (errno != EEXIST) {
+			perror("Error creating named pipe");
+			exit (1);
+		}
+	}
+
+	// set up semaphores
+	sem_sig = sem_open("/sem_sig", O_CREAT, 0644, 0);
+	sem_child_ready = sem_open("/sem_child_ready", O_CREAT, 0644, 0);
+	sem_parent_done = sem_open("/sem_parent_done", O_CREAT, 0644, 0);
+
+	// set up signal handler
+	signal(SIGUSR1, handler);
+
+	puts("!!!Hello World - I'm the master!!!");
+	// set up seed
+	srand(1);
+
+	pid_t pid = fork();
+	if(pid==-1) {
+		perror("Error creating child");
+		exit (1);
+	}
+	else if(pid==0) {
+		sem_post(sem_child_ready); // signal that the child is ready
+
+		sem_wait(sem_parent_done); // wait until the parent is done
+
+		printf("Opening FIFO, waiting for slave to be ready...\n");
+
+		int fd = open("/tmp/named_pipe", O_WRONLY);
+		if(fd==-1) {
+			perror("Error opening named pipe");
+			exit (1);
+		}
+
+		printf("Opened named pipe, slave's ready\n");
+
+		int payload, // the payload is the number to send to slave
+			accumulator=0; // to keep track of the total sum of sent numbers
+
+		while(!terminated) {
+			payload = rand()%MAX;
+			accumulator+=payload;
+
+			sem_wait(sem_sig); // wait for signal from kill command
+
+			printf("payload: %d; accumulator: %d\n", payload, accumulator);
+
+			if (write(fd, &payload, 1) == -1){ // send payload to slave
+				perror("Error writing to pipe");
+				exit (1);
+			}
+
+			if(accumulator>THRESHOLD) {
+				terminated = TRUE; // terminate successfully
+			}
+		}
+
+		close(fd); // close fifo
+		puts("All done. Bye!");
+	}
+	else {
+		sem_wait(sem_child_ready); // wait until the child is ready
+
+		printf("Type <kill -%d %d> to send payload\n", SIGUSR1, pid);
+
+		sem_post(sem_parent_done); // signal that the parent is done
+
+		wait(NULL); // wait for child to terminate
+
+		puts("Child terminated. Bye!");
+	}
+
+	// tidy up semaphores
+	sem_close(sem_sig);
+	sem_close(sem_child_ready);
+	sem_close(sem_parent_done);
+	sem_unlink("/sem_sig");
+	sem_unlink("/sem_child_ready");
+	sem_unlink("/sem_parent_done");
+	sem_unlink("/sem_child_done");
+
+	return EXIT_SUCCESS;
+}
+
---
+++ Test.c
@@ -0,0 +1,150 @@
+/*
+ ============================================================================
+ Name        : A1-Slave.c
+ Author      : Andrea Alboni
+ Version     : 1
+ Copyright   : For personal use only
+ Description : Template for A1's slave program
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define TRUE 1
+#define N_WRITERS 5
+#define BUF_SIZE 10
+
+int buffer[BUF_SIZE]; // threads will have to write here
+int terminated; // to terminate writers
+int n = 0; // number read from master
+int received = 0; // number of writers that have written to buffer
+
+sem_t mutex, barrier;
+pthread_cond_t cond;
+pthread_mutex_t m;
+
+void display_buffer() {
+	int i;
+	printf("Buffer:\n|");
+	for(i=0;i<BUF_SIZE;i++) {
+		printf(" %d\t|",buffer[i]);
+	}
+	puts("");
+}
+
+void *writer(void *arg) {
+
+	while(!terminated) {
+		int target=rand()%BUF_SIZE;
+
+		// update buffer[target]
+		pthread_mutex_lock(&m);
+
+			while (received){ 
+				printf("Waiting for signal\n");
+				pthread_cond_wait(&cond, &m); }
+			//sem_wait(&mutex);
+			received--;
+			buffer[target] += n;
+			printf("Summing %d, updated buffer[%d] to %d\n", n, target, buffer[target]);
+			display_buffer();
+			//sem_post(&mutex);
+
+			while (received == 0){ 
+				pthread_cond_wait(&cond, &m); }
+			buffer[target] += n;
+			received--;
+			//printf("Summing %d, updated buffer[%d] to %d\n", n, target, buffer[target]);
+			//display_buffer();
+		pthread_mutex_unlock(&m);		
+	
+	}
+	puts("Writer terminated. Bye!");
+
+	pthread_exit(NULL);
+}
+
+int main(void) {
+	sem_init(&mutex, 0, 1);
+	pthread_mutex_init(&m, NULL);
+	pthread_cond_init(&cond, NULL);
+
+	pthread_t tid[N_WRITERS];
+	int i, accumulator=0;
+
+	// sanity check
+	if(N_WRITERS<1) {
+		puts("Bye!");
+		exit(1);
+	}
+
+	puts("!!!Hello World - I'm the slave!!!"); /* prints !!!Hello World!!! */
+
+	// set up seed
+	srand(100);
+
+	// set up communication with master
+	int fd = open("/tmp/named_pipe", O_RDONLY);
+	printf("Opening FIFO, waiting for master to be ready...\n");
+	if(fd==-1) {
+		perror("Error opening named pipe");
+		exit (1);
+	}
+	printf("Opened named pipe, master's ready\n");
+
+	// create threads
+	for(i=0;i<N_WRITERS;i++) {
+		if (pthread_create(&tid[i],NULL,writer,NULL) != 0) {
+			perror("Error creating thread");
+			exit(1);
+		}
+	}
+	
+	// enter wait loop
+	while(!terminated) {
+		// read number from master
+		if(read(fd, &n, sizeof(int)) == -1) {
+			perror("Error reading from pipe");
+			exit (1);
+		}
+		// if has read a number, let all threads update the buffer
+		received = N_WRITERS;
+		printf("Received %d\n", n);
+		pthread_cond_broadcast(&cond);
+	
+		// if instead there are no more numbers to read, terminate
+		accumulator += n;
+		
+		if (accumulator >= 100){
+			terminated=TRUE;
+			puts("All done. Bye!");
+		}
+	}
+
+	// tidy up
+	for(i=0;i<N_WRITERS;i++) {
+		if (pthread_join(tid[i],NULL) != 0) {
+			perror("Error joining thread");
+			exit(1);
+		}
+	}
+
+	close(fd); // close fifo
+	pthread_mutex_destroy(&m); // destroy mutex
+
+	// display content of buffer
+	display_buffer();
+
+	return EXIT_SUCCESS;
+}
+
+
